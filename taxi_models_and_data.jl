6f4568e937f39a7a8c251a02d3461430ad4d5b33
Anthony Corso <anthonycorso92@gmail.com>
2021-02-10 10:18:58 -0800

    Working configuration of cGans for both datasets

--- taxi_models_and_data.jl
+++ taxi_models_and_data.jl
@@ -14,17 +14,17 @@ function TaxiDConv(s::Settings)
 	d_common = Chain(
 					 Conv((3,3), 2=>64, pad=1, stride=1),
 					 BatchNorm(64, leakyrelu),
-			 		 Conv((4,4), 64=>64, pad=1, stride=2, leakyrelu),
+			 		 Conv((4,4), 64=>64, pad=1, stride=2),
 					 BatchNorm(64, leakyrelu),
-					 Conv((3,3), 64=>128, pad=1, stride=1, leakyrelu),
+					 Conv((3,3), 64=>128, pad=1, stride=1),
 					 BatchNorm(128, leakyrelu),
-					 Conv((4,4), 128=>128, pad=1, stride=2, leakyrelu),
+					 Conv((4,4), 128=>128, pad=1, stride=2),
 					 BatchNorm(128, leakyrelu),
-					 Conv((3,3), 128=>256, pad=1, stride=1, leakyrelu),
+					 Conv((3,3), 128=>256, pad=1, stride=1),
 					 BatchNorm(256, leakyrelu),
-					 Conv((4,4), 256=>256, pad=1, stride=2, leakyrelu),
+					 Conv((4,4), 256=>256, pad=1, stride=2),
 					 BatchNorm(256, leakyrelu),
-			  		 Conv((3,3), 256=>512, pad=1, stride=1, leakyrelu),
+			  		 Conv((3,3), 256=>512, pad=1, stride=1),
 			  		 x -> reshape(x, 1024, :),
 			  		 Dense(1024, 1)
 					 ) |> gpu
@@ -134,13 +134,18 @@ function Taxi_input(s::Settings)
 end
 
 function gen_Taxi_images(s::Settings)
-	# fn = "KJ_DownsampledTrainingData.h5"
-
 	# Load the data
-	images = h5read(fn, "X_train")
-	y = Float32.(h5read(fn, "y_train"))
-	y[1,:] ./ std(y[1,:])
-	y[2,:] ./ std(y[2,:])
+	# fn = "KJ_DownsampledTrainingData.h5"
+	# images = h5read(fn, "X_train")
+	# y = Float32.(h5read(fn, "y_train"))
+	
+	fn = "SK_DownsampledGANFocusAreaData.h5"
+	images = h5read(fn, "y_train") # yes I know the labels seem backwards
+	y = h5read(fn, "X_train")[1:2, :]
+	
+	y[1,:] ./= std(y[1,:])
+	y[2,:] ./= std(y[2,:])
+	println("extrema of dim1: ", extrema(y[1,:]), " extrema of dim2: ", extrema(y[2,:]))
 	y = y |> gpu
 
 	# Generate fixed set of labels and save the corresponding images
@@ -152,7 +157,7 @@ function gen_Taxi_images(s::Settings)
 	real_images = [cpu(images[:,:, i]) for i in indices]
 	image_array = permutedims(reduce(vcat, reduce.(hcat, partition(real_images, s.output_y))), (2,1))
 	image_array = Gray.(image_array)
-	save("real_images.png", image_array)
+	save("real_images_sydney.png", image_array)
 
 	# rescale the data
 	images = reshape(2f0 .* images .- 1f0, 16, 8, 1, :) |> gpu # Normalize to [-1, 1]
@@ -161,16 +166,14 @@ function gen_Taxi_images(s::Settings)
 	data, fixed_noise, fixed_labels
 end
 
-images = h5read("SK_DownsampledGANFocusAreaData.h5", "X_train")
 
+train(Settings(G = TaxiGConv, D = TaxiDConv, rand_input = Taxi_input, loss = DCGANLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "kjTaxi_DCGAN"))
+train(Settings(G = TaxiGConv, D = TaxiDConvSpectral, rand_input = Taxi_input, loss = DCGANLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "kjTaxi_DCGAN_Spectral"))
 
-train(Settings(G = TaxiGConv, D = TaxiDConv, rand_input = Taxi_input, loss = DCGANLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "Taxi_DCGAN"))
-train(Settings(G = TaxiGConv, D = TaxiDConvSpectral, rand_input = Taxi_input, loss = DCGANLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "Taxi_DCGAN_Spectral"))
+train(Settings(G = TaxiGConv, D = TaxiDConv, rand_input = Taxi_input, loss = LSLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "kjTaxi_LSGAN"))
+train(Settings(G = TaxiGConv, D = TaxiDConvSpectral, rand_input = Taxi_input, loss = LSLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "kjTaxi_LSGAN_Spectral"))
 
-train(Settings(G = TaxiGConv, D = TaxiDConv, rand_input = Taxi_input, loss = LSLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "Taxi_LSGAN"))
-train(Settings(G = TaxiGConv, D = TaxiDConvSpectral, rand_input = Taxi_input, loss = LSLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "Taxi_LSGAN_Spectral"))
-
-train(Settings(G = TaxiGConv, D = TaxiDConv, rand_input = Taxi_input, loss = HingeLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "Taxi_Hinge"))
-train(Settings(G = TaxiGConv, D = TaxiDConvSpectral, rand_input = Taxi_input, loss = HingeLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "Taxi_Hinge_Spectral"))
+train(Settings(G = TaxiGConv, D = TaxiDConv, rand_input = Taxi_input, loss = HingeLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "kjTaxi_Hinge"))
+train(Settings(G = TaxiGConv, D = TaxiDConvSpectral, rand_input = Taxi_input, loss = HingeLoss(), img_fun = gen_Taxi_images, nclasses = 2, output_dir = "kjTaxi_Hinge_Spectral"))
 
 
